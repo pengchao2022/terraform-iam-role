cf99ee798b777e093607eb047cadbbd16b0780ce
pengchao2022 <pengchao.ma2@outlook.com>
2026-05-31 10:53:51 +0800

    feat: GTP-54273 modified the code

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name = "MyGitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_role_policy" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_role_arn" {
+  value = aws_iam_role.github_actions_role.arn
+}
+
+output "github_role_name" {
+  value = aws_iam_role.github_actions_role.name
+}
+
+output "oidc_provider_arn" {
+  value = aws_iam_openid_connect_provider.github_actions.arn
+}
